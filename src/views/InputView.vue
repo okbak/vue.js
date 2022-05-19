b50c42685f18a69c4920ec6808ee09580ac011a4
Sergiu Mocanu <43932122+okbak@users.noreply.github.com>
2022-05-19 22:59:50 +0300

    commit init

--- src/views/InputView.vue
+++ src/views/InputView.vue
@@ -2,4 +2,9 @@
   <div class="input">
     <h1>This is an input page</h1>
   </div>
+      <input v-model="message" placeholder="x" />
+    <p>Введённое сообщение: {{ message }}</p>
+    <input v-model="message" placeholder="y" />
+    <p>Введённое сообщение: {{ message }}</p>
+  </div>
 </template>
